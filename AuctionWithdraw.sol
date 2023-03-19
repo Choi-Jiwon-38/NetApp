2ee7156b4cb778f596feb7c7982667811f03a862
Choi-Jiwon-38 <chlrhwldnjs@naver.com>
2023-03-19 17:54:19 +0900

    fix: AuctionWithdraw 파일 수정

--- AuctionWithdraw.sol
+++ AuctionWithdraw.sol
@@ -1,41 +1,33 @@
-pragma solidity ^0.4.11;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.4.11;
 contract AuctionWithdraw {
-	address public highestBidder;	// �ְ� ������ ��巹��
-	uint public highestBid;	// �ְ� ������
-	mapping(address => uint) public userBalances;	// ��ȯ�� �׼��� �����ϴ� ����
+	address public highestBidder;
+	uint public highestBid;
+	mapping(address => uint) public userBalances;
 	
-	/// ������
-	function AuctionWithdraw() payable {
+	constructor() payable {
 		highestBidder = msg.sender;
 		highestBid = 0;
 	}
 	
-	/// ���� ó�� �Լ�
 	function bid() public payable {
-		// �������� ���� �ְ� �����׺��� ū�� Ȯ��
 		require(msg.value > highestBid);
 
-		// �ְ� ������ ��巹���� ���� ��ȯ�׼� ������Ʈ
 		userBalances[highestBidder] += highestBid;
 				
-		// ������Ʈ ������Ʈ
 		highestBid = msg.value;
 		highestBidder = msg.sender;
 	}
 	
-	function withdraw() public{
-		// ��ȯ�� �׼��� 0���� ū�� Ȯ��
+	function withdraw() public {
 		require(userBalances[msg.sender] > 0);
 		
-		// ��ȯ�� �׼��� ����
 		uint refundAmount = userBalances[msg.sender];
 		
-		// ��ȯ�� ������Ʈ
 		userBalances[msg.sender] = 0;
-		
-		// ������ ��ȯ ó��
-		if(!msg.sender.send(refundAmount)) {
-			throw;
+
+		if(!payable(msg.sender).send(refundAmount)) {
+			revert();
 		}
 	}
 }
